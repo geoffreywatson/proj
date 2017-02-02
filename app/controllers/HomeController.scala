aff96b6260c8b4e52b524a27ce7348239e8215d1
geoffreywatson <geoff777@gmail.com>
2017-02-02 23:18:09 +0000

    made the sign in view

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,7 +1,8 @@
 package controllers
 
 import javax.inject._
-import play.api._
+
+import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
 
 /**
@@ -9,7 +10,7 @@ import play.api.mvc._
  * application's home page.
  */
 @Singleton
-class HomeController @Inject() extends Controller {
+class HomeController @Inject() (val messagesApi:MessagesApi) extends Controller with I18nSupport{
 
   /**
    * Create an Action to render an HTML page with a welcome message.
